a92178fcb412551c50a1500ba0efec21af8fbca1
Harsh Prakash <prakashharsh32@gmail.com>
2021-07-31 20:53:56 +0530

    old design final

--- contracts/Marsmello.sol
+++ contracts/Marsmello.sol
@@ -3,20 +3,22 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
 contract Marsmello is ERC20 {
     constructor() ERC20(m_name, m_symbol) {
         _mint(msg.sender, m_supply * 10**decimals());
         deploy_time = block.timestamp;
+        factories.push(
+            Factory("Void", address(this), 0, 0, 0, CoOrdinates(0, 0))
+        );
     }
 
-    string m_name = "MarsMellow";
+    string m_name = "MarsMello";
     string m_symbol = "MLO";
     uint256 m_supply = 10**9;
     uint256 deploy_time;
     uint256 randomness = 12345678901234567890;
     uint256 land_price = 1000 * 10**decimals();
+    uint256 factory_price = 1000 * 10**decimals();
 
     uint256 factory_count = 0;
     struct Land {
@@ -47,12 +49,12 @@ contract Marsmello is ERC20 {
         uint16[5] ores;
         Factory ontop;
     }
-    address[5] private resources = [
-        0x110472D9B5661DC1d8BA8C637Dbde1f5a813cD8A,
-        0x7155651d11C869f76BE988cf30BE3B856f29E905,
-        0xe8D98e3331C0434D38FfB8041eF2EF233C41Cf70,
-        0x38a3FD50BF7240988639D7DA8A0EcD91052AF1d1,
-        0x8042A576cB35d83cbECE64e211BF0570F72e4Cfd
+    IERC20[5] private tokens = [
+        IERC20(0x110472D9B5661DC1d8BA8C637Dbde1f5a813cD8A),
+        IERC20(0x7155651d11C869f76BE988cf30BE3B856f29E905),
+        IERC20(0xe8D98e3331C0434D38FfB8041eF2EF233C41Cf70),
+        IERC20(0x38a3FD50BF7240988639D7DA8A0EcD91052AF1d1),
+        IERC20(0x8042A576cB35d83cbECE64e211BF0570F72e4Cfd)
     ];
 
     mapping(int256 => mapping(int256 => Land)) private lands;
@@ -86,34 +88,47 @@ contract Marsmello is ERC20 {
         return (land_price);
     }
 
-    function mintFactory(
-        string memory name,
-        uint8 ftype,
-        uint8 efficiency
-    ) public returns (bool) {
+    function mintFactory(string memory name, uint8 ftype) public {
         require(
-            balanceOf(msg.sender) >= 1000,
+            balanceOf(msg.sender) >= factory_price,
             "Not enough MLO in your wallet to buy land !"
         );
-        factories.push(
-            Factory(name, msg.sender, ftype, efficiency, 0, CoOrdinates(0, 0))
+        uint256 seed = uint256(
+            keccak256(
+                abi.encodePacked(
+                    randomness,
+                    blockhash(block.number - 1),
+                    factories.length
+                )
+            )
         );
+        factories.push(
+            Factory(
+                name,
+                msg.sender,
+                ftype,
+                uint64(51 + (seed % 50)),
+                0,
+                CoOrdinates(0, 0)
+            )
+        );
+        _transfer(msg.sender, address(this), factory_price);
         users[msg.sender].factories.push(factories.length - 1);
-        return true;
     }
 
     function calcOreDist(uint256 seed) public pure returns (uint16[5] memory) {
         return [
-            uint16(23),
-            uint16(17),
-            uint16(13),
-            uint16(10),
+            uint16(15 + (seed % 13)),
+            uint16(5 + (seed % 11)),
+            uint16(5 + (seed % 7)),
+            uint16(3 + (seed % 5)),
             uint16(seed % 5)
         ];
     }
 
-    function mintLand(int256 x, int256 y) public returns (bool) {
-        require(x != 0 && y != 0, "Can't buy spawn !");
+    function mintLand(int256 x, int256 y) public {
+        require(x != 0 || y != 0, "Can't buy spawn !");
+        require(x < 1000000000 || y != 0, "Can't buy spawn !");
         require(lands[x][y].owner == address(0x0), "Land already exists !");
         require(
             balanceOf(msg.sender) >= land_price,
@@ -122,11 +137,16 @@ contract Marsmello is ERC20 {
         _transfer(msg.sender, address(this), land_price);
         land_price += land_price / 100;
         uint256 seed = uint256(
-            keccak256(abi.encodePacked(randomness, block.timestamp, land_price))
+            keccak256(
+                abi.encodePacked(
+                    randomness,
+                    blockhash(block.number - 1),
+                    land_price
+                )
+            )
         );
         lands[x][y] = Land(msg.sender, seed, calcOreDist(seed), 0);
         users[msg.sender].lands.push(CoOrdinates(x, y));
-        return true;
     }
 
     function _clearLand(CoOrdinates memory c) private {
@@ -144,21 +164,20 @@ contract Marsmello is ERC20 {
         uint256 factory_id,
         int256 x,
         int256 y
-    ) public landOwner(x, y) factoryOwner(factory_id) returns (bool) {
+    ) public landOwner(x, y) factoryOwner(factory_id) {
         _clearFactory(lands[x][y].ontop);
         _clearLand(factories[factory_id].placedon);
 
         lands[x][y].ontop = factory_id;
         factories[factory_id].placedon = CoOrdinates(x, y);
         factories[factory_id].lastclaimed = block.timestamp;
-        return true;
     }
 
     function transferLand(
         address to,
         int256 x,
         int256 y
-    ) public landOwner(x, y) returns (bool) {
+    ) public landOwner(x, y) {
         _clearFactory(lands[x][y].ontop);
         _clearLand(CoOrdinates(x, y));
         lands[x][y].owner = to;
@@ -172,7 +191,6 @@ contract Marsmello is ERC20 {
             }
         }
         users[to].lands.push(CoOrdinates(x, y));
-        return true;
     }
 
     function transferFactory(address to, uint256 factory_id)
@@ -192,25 +210,34 @@ contract Marsmello is ERC20 {
         users[to].factories.push(factory_id);
     }
 
-    // function claimAll() public {
-    //     uint256[5] memory amounts;
-    //     uint64 t;
-    //     for (uint256 i = 0; i < users[msg.sender].factories.length; i++) {
-    //         Factory storage f = factories[users[msg.sender].factories[i]];
-    //         if (f.placedon.x != 0 || f.placedon.y != 0) {
-    //             t = f.ftype % 10;
-    //             if (t > 0) {
-    //                 t--;
-    //                 amounts[t] +=
-    //                     (lands[f.placedon.x][f.placedon.y].ores[t] *
-    //                         f.efficiency *
-    //                         10**18) /
-    //                     36;
-    //             }
-    //         }
-    //     }
-    //     for (uint256 i = 0; i < 5; i++) {}
-    // }
+    function claimAll() public {
+        uint256[5] memory amounts;
+        uint64 t;
+        for (uint256 i = 0; i < users[msg.sender].factories.length; i++) {
+            Factory storage f = factories[users[msg.sender].factories[i]];
+            if (f.placedon.x != 0 || f.placedon.y != 0) {
+                t = f.ftype % 10;
+                if (t > 0) {
+                    t--;
+                    amounts[t] +=
+                        (lands[f.placedon.x][f.placedon.y].ores[t] *
+                            f.efficiency *
+                            10**18) /
+                        36;
+                }
+            }
+        }
+        for (uint256 i = 0; i < 5; i++) {
+            if (amounts[i] > 0)
+                require(tokens[i].transfer(msg.sender, amounts[i]));
+        }
+        for (uint256 i = 0; i < users[msg.sender].factories.length; i++) {
+            Factory storage f = factories[users[msg.sender].factories[i]];
+            if (f.placedon.x != 0 || f.placedon.y != 0) {
+                f.lastclaimed = block.timestamp;
+            }
+        }
+    }
 
     function getArea(int256 x, int256 y)
         public
